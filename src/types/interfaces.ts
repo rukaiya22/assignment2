feeee994ca0c7c190933477ba7bdc20a8eb5eb06
rukaiya22 <rukaiyarafique2000@gmail.com>
2025-04-16 13:41:35 +0100

    Movie Details Page - included production countries.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -19,12 +19,33 @@ export interface BaseMovieProps {
 
 export interface BaseMovieListProps {
     movies: BaseMovieProps[];
-}    
+}
 
 export interface MovieDetailsProps extends BaseMovieProps {
     genres: {
-      id: number;
-      name: string;
+        id: number;
+        name: string;
     }[];
-  }
+    production_countries:
+    {
+        iso_3166_1: string,
+        name: string,
+    }[];
+
+}
+
+export interface MovieImage {
+    file_path: string;
+    aspect_ratio?: number; //some props are optional...
+    height?: number;
+    iso_639_1?: string;
+    vote_average?: number;
+    vote_count?: number;
+    width?: number;
+}
+
+export interface MoviePageProps {
+    movie: MovieDetailsProps;
+    images: MovieImage[];
+}
 
